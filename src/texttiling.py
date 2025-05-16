7907aacf32a4ecd5deb2ab9c094d1a2443df0fc7
Saint-Clair Lima <caehevanras@gmail.com>
2025-05-16 10:40:17 -0300

    CRia arquivos de teste e base para segmentador semântico definitivo

--- src/texttiling.py
+++ src/texttiling.py
@@ -1,20 +1,24 @@
 
 # importing necessary libraries and modules
-from difflib import SequenceMatcher
 import math
 import spacy
-import string
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.colors as mcolors
 import networkx as nx
 import community as community_louvain
 from collections import defaultdict
-import utils as ut
 from transformers import AutoTokenizer, AutoModel
 import torch
 from scipy.spatial.distance import cosine
 from bert_score import score
+
+def find_overlap(vector1, vector2):
+    min_v1, max_v1 = min(vector1), max(vector1)
+    min_v2, max_v2 = min(vector2), max(vector2)
+
+    one_in_two = [num for num in vector1 if min_v2 <= num <= max_v2]
+    two_in_one = [num for num in vector2 if min_v1 <= num <= max_v1]
+    overlap = one_in_two+two_in_one
+
+    return overlap, len(one_in_two), len(two_in_one)
 
 def get_similarity_scores(vec_a, vec_b, model):
     """
@@ -33,35 +37,19 @@ def get_similarity_scores(vec_a, vec_b, model):
     vec_a = [x.lower() for x in vec_a]  # convert vector a to lowercase
     vec_b = [x.lower() for x in vec_b]  # convert vector b to lowercase
 
-    if model == "bert":
+    if model == "bertscore":
         # BERTScore returns three values: Precision, Recall, and F1 Score
         # Here we use F1 Score as the similarity measure
-        _, _, f1_score = score(vec_a, vec_b, lang='en', model_type='bert-base-uncased')
+        _, _, f1_score = score(vec_a, vec_b, lang='pt', model_type='xlm-roberta-base')
 
         # f1_score is a tensor with the F1 score for each pair of sentences.
         # Since we only have one pair, we take the first (and only) element.
         similarities = [f1_score[i].item() for i in range(len(vec_a))]
-    elif model == "seqmatch":
-        translation_table = str.maketrans('', '', string.punctuation)
-        similarities = []
-        for xa, xb in zip(vec_a, vec_b):
-            xa = xa.translate(translation_table)
-            xb = xb.translate(translation_table)
-            sm = SequenceMatcher(None, xa, xb).ratio()
-            similarities.append(sm)
-    elif model == "jaccard":
-        translation_table = str.maketrans('', '', string.punctuation)
-        similarities = []
-        for xa, xb in zip(vec_a, vec_b):
-            a_words = set(xa.translate(translation_table).split())
-            b_words = set(xb.translate(translation_table).split())
-            js = len(a_words & b_words) / len(a_words | b_words)
-            similarities.append(js)
     else: 
         # any transformer chosen from HuggingFace can be used here
         similarities = []       
-        tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/"+model)
-        model = AutoModel.from_pretrained("sentence-transformers/"+model)
+        tokenizer = AutoTokenizer.from_pretrained(model)
+        model = AutoModel.from_pretrained(model)
         embeddings_a = []
         embeddings_b = []
 
@@ -103,7 +91,7 @@ def compact_clusters(clusters):
         for i in range(len(clusters)):
             target_cl = clusters[i]
             # find_overlap() returns the range overlaps and number of overlaps between two clusters
-            overlap, n_1_in_2, n_2_in_1 = ut.find_overlap(target_cl, curr_cl)
+            overlap, n_1_in_2, n_2_in_1 = find_overlap(target_cl, curr_cl)
             if overlap:
                 # The code block here decides which cluster to merge the overlapping elements into.
                 # It aims to minimize the amount of element transfer. If it's equally easy to merge into both,
@@ -169,9 +157,9 @@ def create_similarity_graph(sentences, K, model):
     # The final result is a list of weighted edges in the similarity graph.
     return result
 
-def create_tiles(doc_path, model='bert'):
+def create_tiles(text, model='jvanhoof/all-MiniLM-L6-multilingual-v2-en-es-pt-pt-br-v2'):
     """
-    This function reads in a document, segments it into sentences, and groups similar sentences into 'tiles' or clusters. 
+    This function segments a text into sentences, and groups similar sentences into 'tiles' or clusters. 
     Each tile represents a section of the text where the sentences are considered similar based on the similarity model used. 
     The function then removes overlaps from these tiles and returns the text tiling in the form [0, th1, th2.... thN, len(sentences)-1] 
     
@@ -182,9 +170,6 @@ def create_tiles(doc_path, model='bert'):
     Returns:
     - a vector containing the list of thresholds representing the tiling for input document.
     """
-    # Open and read the text file
-    with open(doc_path, 'r',  encoding='utf-8') as file:
-        text =  file.read()
     text = text.replace('\n', ' ')
 
     # Process the text to get a Doc object
@@ -192,7 +177,7 @@ def create_tiles(doc_path, model='bert'):
     doc = nlp(text)
     # Extract sentences from the Doc object
     sentences = [sent.text for sent in doc.sents]
-
+    
     # Create a similarity graph based on the extracted sentences
     graph = create_similarity_graph(sentences, 5, model=model)
 
@@ -217,133 +202,8 @@ def create_tiles(doc_path, model='bert'):
     tiles = compact_clusters(tiles)
 
     # Generate the text tiling in the form [0, th1, th2.... thN, len(sentences)-1] 
-    return [c[0] for c in tiles]+[tiles[-1][-1]]
+    tiles_idxs = [c[0] for c in tiles]+[tiles[-1][-1]]
+    tiled_text = ['\n'.join(sentences[tiles_idxs[idx] : tiles_idxs[idx+1]]) for idx in range(len(tiles_idxs) - 1)]
+    return tiles_idxs, tiled_text
 
-def plot_tiles(tiles, labels):
-    """
-    This function generates a stacked bar plot for a list of tile vectors.
-    
-    Parameters:
-    - tiles: A list of tile vectors. Each tile vector is a list of numeric values.
-    - labels: A list of labels for the tile vectors, typically representing the names or identifiers of the vectors.
-
-    This function generates a distinct color for each segment of the first vector (master vector). 
-    For the other vectors, the color of the segments depends on which thresholds of the master vector 
-    they fall between. This helps to visually compare the distributions of the different vectors.
-
-    This function doesn't return anything; it directly creates a plot using matplotlib.
-    """
-
-    # Generate distinct colors
-    colors = list(mcolors.TABLEAU_COLORS.values())[:10] 
-    # Number of vectors
-    N = len(tiles)
-    # The x locations for the vectors
-    ind = np.arange(N)
-
-    # The width of the bars
-    width = 0.7
-    # Figure and axis
-    fig, ax = plt.subplots()
-    # Master vector is the first vector
-    master_vector = tiles[0]
-
-    # Iterate over each vector
-    for i in range(N):
-        # Initialize the bottom for the stacking purpose
-        bottom = 0
-        # Iterate over each element in vector
-        for j in range(len(tiles[i])):
-            # Create a bar from bottom to the value
-
-            # Find the color based on the master thresholds
-            if i == 0:
-                color_index = j
-            else:
-                color_index = 0
-                for k in range(len(master_vector)):
-                    if k != 0 and tiles[i][j] > master_vector[k] and tiles[i][j-1] < master_vector[k]:
-                        color_index = k
-                        break
-                    elif tiles[i][j] <= master_vector[k]:
-                        color_index = k
-                        break
-
-            # Create the bar with the selected color
-            ax.bar(i, tiles[i][j] - bottom, bottom=bottom, color=colors[color_index%10], edgecolor='black', width=width)
-            # Update the bottom for the next bar
-            bottom = tiles[i][j]
-
-    # Add labels at the x ticks
-    plt.xticks(ind, labels, rotation='horizontal')
-    # You can uncomment the following line if you want to keep the y ticks.
-    # plt.yticks([])
-
-    # Show the plot
-    plt.show()
-
-def tiling_score(master, candidate):
-    """
-    This function calculates the tiling score between a master list and a candidate list. 
-    The tiling score represents how well the candidate list can approximate the master list 
-    while taking into account the order and the number of elements.
-
-    Parameters:
-    - master: The master list of numbers which the candidate list will be compared against.
-    - candidate: The candidate list of numbers which will be used to approximate the master list.
-
-    Returns:
-    - tiling_score: A score between 0 and 1 representing the degree of similarity between the master list and the candidate list. 
-      A score closer to 1 means the candidate list can better approximate the master list.
-
-    The function works in the following way:
-    1. For each element in the master list (except the last one), it finds the closest value in the candidate list.
-    2. It calculates the total distance based on how close the selected elements in the candidate list are to the corresponding elements in the master list.
-    3. It normalizes this total distance by the last element in the master list.
-    4. It also considers a penalty based on the difference in the length between the master list and the candidate list.
-    5. Finally, it calculates the tiling score based on these distances and penalties.
-    """
-    threshold_vec = []
-    for m in master[:-1]:
-        distances = [abs(m-c) for c in candidate]
-        min_distance_index = np.argmin(distances)
-        closest_value = candidate[min_distance_index]
-        
-        # If the closest value is already in the result list, then we put None
-        if closest_value in threshold_vec or closest_value == master[-1]:
-            threshold_vec.append(None)
-        else:
-            threshold_vec.append(closest_value)
-    threshold_vec.append(master[-1])
-
-    distance = 0
-    for i in range(len(threshold_vec)):
-        if threshold_vec[i] == None:
-            distance += (master[i]-master[i-1])
-        else:
-            distance += abs(master[i]-threshold_vec[i])
-    distance = distance/master[-1]
-
-    num_penalty = np.exp(-(len(candidate)-len(master))/1000)
-    return (1-distance)*num_penalty
-
-models = {
-    "bert":"BERT Score",
-    "paraphrase-MiniLM-L6-v2":"paraphrase-MiniLM-L6-v2"
-}
-
-labels = []
-original = [0, 62, 91, 129, 160]
-tiles = []
-for model in models:
-    tl = create_tiles("doc.txt", model)
-    tiles += [tl]
-    labels.append(models[model])
-    print("%s score: %.4f" %(labels[-1], tiling_score(original, tl)))
-
-    print (tiles)
-
-plot_tiles([original]+tiles, ["Original"]+labels)
-with open('doc.txt', 'r', encoding='urf-8') as arq:
-    d = arq.read().split('')
 
