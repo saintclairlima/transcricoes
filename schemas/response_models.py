827f91d3b0712701e4f4c0285d03d15efcde05a2
Saint-Clair Lima <caehevanras@gmail.com>
2026-05-29 19:00:44 -0300

    Refactor TranscricaoResponse attribute names
    Refactor TranscricaoResponse attributes to use snake_case naming convention and set default values to None.

--- schemas/response_models.py
+++ schemas/response_models.py
@@ -3,21 +3,21 @@ from typing import List, Optional
 
 
 class TranscricaoResponse(BaseModel):
-    idMarcador: Optional[int]
-    idDeputado: Optional[int]
-    nomeDeputado: Optional[str]
-    texto: Optional[str]
-    idVideo: Optional[str]
-    tempoInicial: Optional[float]
-    tempoFinal: Optional[float]
-    dataInclusao: Optional[str]
-    idsSegmentos: Optional[str]
-    chaveFase: Optional[str]
-    tituloFase: Optional[str]
-    sentimento: Optional[str]
-    tomDiscurso: Optional[str]
-    temas: Optional[str]
-    numPalavras: Optional[int]
+    id_marcador: Optional[int] = None
+    id_deputado: Optional[int] = None
+    nome_deputado: Optional[str] = None
+    texto: Optional[str] = None
+    id_video: Optional[str] = None
+    tempo_inicial: Optional[float] = None
+    tempo_final: Optional[float] = None
+    data_inclusao: Optional[str] = None
+    ids_segmentos: Optional[str] = None
+    chave_fase: Optional[str] = None
+    titulo_fase: Optional[str] = None
+    sentimento: Optional[str] = None
+    tom_discurso: Optional[str] = None
+    temas: Optional[str] = None
+    num_palavras: Optional[int] = None
     cosine_distance: float
 
 
